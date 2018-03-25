e566e7eff5d9345da26f7a92e2e9cfde81682d87
cjgilmor <36717120+cjgilmor@users.noreply.github.com>
2018-03-25 08:01:03 -0400

    Upload Files
    NEW: forms.py, models.py, urls.py, views.py

---
+++ forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from .models import UFoodDB
+from django import forms
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+class FoodClass(forms.ModelForm):
+    
+    class Meta:
+        model = UFoodDB
+        fields = ['ID', 'ProductID', 'Size', 'Produce']
---
+++ models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class UFoodDB(models.Model):
+    ID = models.CharField(max_length=100)
+    ProductID = models.TextField(max_length=200)
+    Size = models.TextField(max_length=500)
+    Produce = models.TextField(max_length=500)
+        
+    def __unicode__(self):
+        return self.title
