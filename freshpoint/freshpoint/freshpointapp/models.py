3b41b3edeb9f2dad9b3b03a3ed789bbf2630dec6
OMKNO <Dasani@Dars-MacBook-Pro.local>
2018-03-27 08:43:06 -0400

    making sure master has proper changes

--- freshpoint/freshpoint/freshpointapp/models.py
+++ freshpoint/freshpoint/freshpointapp/models.py
@@ -5,9 +5,9 @@ from django.db import models
 
 class UFoodDB(models.Model):
     ID = models.CharField(max_length=100, primary_key=True)
-    ProductID = models.TextField(max_length=200)
-    Size = models.TextField(max_length=500)
-    Produce = models.TextField(max_length=500)
+    ProductID = models.TextField(max_length=200,blank=True)
+    Size = models.TextField(max_length=500,blank=True)
+    Produce = models.TextField(max_length=500,blank=True)
         
     def __unicode__(self):
         return self.title
